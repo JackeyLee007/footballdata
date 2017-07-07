c2d73f9a8fb45cffa155ad1442d240532083ce68
i070695 <Pengcheng.Li01@sap.com>
2017-07-07 16:29:41 +0800

    Crawl all team match data

--- football/items.py
+++ football/items.py
@@ -14,6 +14,54 @@ class FootballItem(scrapy.Item):
     pass
 
 
+class MatchItem(scrapy.Item):
+    date = scrapy.Field()
+    compet = scrapy.Field()
+    game = scrapy.Field()
+    result = scrapy.Field()
+    goal = scrapy.Field()
+    lose = scrapy.Field()
+    assist = scrapy.Field()
+    passball = scrapy.Field()
+    steal = scrapy.Field()
+    offside = scrapy.Field()
+    foul = scrapy.Field()
+    red = scrapy.Field()
+    yellow = scrapy.Field()
+
+    # 射门
+    shoot       = scrapy.Field()
+    #射正
+    targetShoot = scrapy.Field()
+    # 射正率
+    targetRate  = scrapy.Field()
+    # 成功率
+    successRate = scrapy.Field()
+    # 头球进球
+    headScore   = scrapy.Field()
+    # 直接任意球进球
+    directFree  = scrapy.Field()
+    # 点球
+    penaltyKick = scrapy.Field()
+    # 赢得点球
+    penaltyScore= scrapy.Field()
+    #=============================
+    # 拦截
+    intercept   = scrapy.Field()
+    # 解围
+    clearance   = scrapy.Field()
+    # 头球解围
+    headClear   = scrapy.Field()
+    # 后场解围
+    backClear   = scrapy.Field()
+    # 头球争顶成功
+    headDuel    = scrapy.Field()
+    # 头球争顶失败
+    headDuelMiss= scrapy.Field()
+    # 乌龙球
+    ownGoal     = scrapy.Field()
+
+
 class FootballTeamItem(scrapy.Item):
 
     name    = scrapy.Field()
@@ -22,3 +70,4 @@ class FootballTeamItem(scrapy.Item):
     court   = scrapy.Field()
     start   = scrapy.Field()
     coach   = scrapy.Field()
+    matches = scrapy.Field()
--- football/spiders/qqfootball.py
+++ football/spiders/qqfootball.py
@@ -2,9 +2,10 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
 from scrapy.contrib.loader import ItemLoader
 
-from football.items import FootballTeamItem
+from football.items import FootballTeamItem, MatchItem
 
 
 class QqfootballSpider(CrawlSpider):
@@ -19,30 +20,64 @@ class QqfootballSpider(CrawlSpider):
     def parse_item(self, response):
 
         teamItem = FootballTeamItem()
-        teamHeader = response.xpath('//div[contains(@class, "intro-con team-con")]')
-        # teamHeader = response.xpath('//div[@class="intro-con team-con"]/table/tbody')
         teamItem['name'] = response.xpath('//h2[@class="team-logo"]/strong/text()').get()
 
+        teamHeader = response.xpath('//div[contains(@class, "intro-con team-con")]')
+        teamItem['league']  = teamHeader.xpath('.//tr[1]/td[1]/a/text()').get()
+        teamItem['city']    =  teamHeader.xpath('.//tr[1]/td[2]/text()').get()
+        teamItem['coach']   =  teamHeader.xpath('.//tr[1]/td[3]/text()').get()
+        teamItem['start']   =  teamHeader.xpath('.//tr[2]/td[1]/text()').get()
+        teamItem['court']   =  teamHeader.xpath('.//tr[2]/td[2]/text()').get()
 
-        teamItem['league'] = teamHeader.xpath('.//tr[1]/td[1]/a/text()').get()
+        teamItem['matches'] = []
+        for (match, attack, defense) in zip(response.xpath('//*[@id="table22_con_0"]/table/tbody/tr'),
+                                            response.xpath('//*[@id="table22_con_1"]/table/tbody/tr'),
+                                            response.xpath('//*[@id="table22_con_2"]/table/tbody/tr'),):
+            mi = MatchItem()
+            mi['date']      = match.xpath('.//td[1]/text()').get()
+            mi['compet']    = match.xpath('.//td[2]/text()').get()
+            mi['game']      = match.xpath('.//td[3]/a/text()').get()
+            mi['result']    = match.xpath('.//td[4]/text()').get()
+            mi['goal']      = match.xpath('.//td[5]/text()').get()
+            mi['lose']      = match.xpath('.//td[6]/text()').get()
+            mi['assist']    = match.xpath('.//td[7]/text()').get()
+            mi['passball']  = match.xpath('.//td[8]/text()').get()
+            mi['steal']     = match.xpath('.//td[9]/text()').get()
+            mi['offside']   = match.xpath('.//td[10]/text()').get()
+            mi['foul']      = match.xpath('.//td[11]/text()').get()
+            mi['red']       = match.xpath('.//td[12]/text()').get()
+            mi['yellow']    = match.xpath('.//td[13]/text()').get()
+
+            mi['shoot']     = attack.xpath('.//td[4]/text()').get()
+            mi['targetShoot']= attack.xpath('.//td[5]/text()').get()
+
+            mi['targetRate']= attack.xpath('.//td[6]/text()').get()
+
+            mi['successRate']= attack.xpath('.//td[7]/text()').get()
+
+            mi['headScore']= attack.xpath('.//td[8]/text()').get()
+
+            mi['directFree']= attack.xpath('.//td[9]/text()').get()
+
+            mi['penaltyKick']= attack.xpath('.//td[10]/text()').get()
+
+            mi['penaltyScore']= attack.xpath('.//td[11]/text()').get()
 
 
-        ft = FootballTeamItem()
-        headerLoader = ItemLoader(ft, response = response)
-        headerLoader.add_xpath('name',  '//h2[@class="team-logo"]/strong/text()')
-        headerLoader.add_xpath('league','//div[contains(@class, "intro-con team-con")]/table/tbody/tr[1]/td[1]/a/text()')
-        headerLoader.add_xpath('coach', '//div[contains(@class, "intro-con team-con")]/table/tbody/tr[1]/td[2]/text()')
-        headerLoader.add_xpath('city',  '//div[contains(@class, "intro-con team-con")]/table/tbody/tr[1]/td[3]/text()')
-        headerLoader.add_xpath('start', '//div[contains(@class, "intro-con team-con")]/table/tbody/tr[2]/td[1]/text()')
-        headerLoader.add_xpath('court', '//div[contains(@class, "intro-con team-con")]/table/tbody/tr[2]/td[2]/text()')
-        headerLoader.load_item()
+            mi['intercept']= defense.xpath('.//td[4]/text()').get()
+
+            mi['clearance']= defense.xpath('.//td[5]/text()').get()
 
 
-        # matchs = response.xpath('//div[@id="table22_con_0"]/table/tbody/tr[1]/td[3]/a/text()').get()
-        matchs = response.xpath('//div[@id="table22_con_0"]/table/tbody/tr[1]/td[3]')
-        r = matchs.xpath('./a/text()').get()
-        ft['name'] = 'a'
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return ft
+            mi['headClear']= defense.xpath('.//td[6]/text()').get()
+
+            mi['backClear']= defense.xpath('.//td[7]/text()').get()
+
+            mi['headDuel']= defense.xpath('.//td[8]/text()').get()
+
+            mi['headDuelMiss']= defense.xpath('.//td[9]/text()').get()
+            mi['ownGoal']= defense.xpath('.//td[10]/text()').get()
+
+            teamItem['matches'].append(mi)
+
+        return teamItem
